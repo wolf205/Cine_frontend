d91fb82b8cc8264b19646648a8abd4518e3de2fd
Nguyen Hoai Nam <nguyenhoainam2005tg@gmail.com>
2026-05-04 15:31:33 +0700

    ket noi api cho register

--- src/pages/Auth/RegisterPage.jsx
+++ src/pages/Auth/RegisterPage.jsx
@@ -1,8 +1,41 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/ui/Button";
 import Input from "../../components/ui/Input";
+import axios from "../../api/axios";
+import { useState } from "react";
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.post("/auth/signUp", formData);
+
+      const isSuccess = response.data.success;
+      // Điều hướng sau khi đăng nhập thành công
+      if (isSuccess) {
+        navigate("/login");
+      }
+    } catch (error) {
+      const errorMessage =
+        error.response?.data?.message || "Đăng ký thất bại. Vui lòng thử lại.";
+      setError(errorMessage);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div className="min-h-screen flex items-center justify-center bg-background px-4">
       {/* Container chính với hiệu ứng Movie Card */}
@@ -15,22 +48,45 @@ const RegisterPage = () => {
           <p className="mt-2 text-text-muted">Vui lòng đăng ký để tiếp tục</p>
         </div>
 
+        {/* --- Hiển thị thông báo lỗi ở đây --- */}
+        {error && (
+          <div className="p-3 rounded-lg bg-red-500/10 border border-red-500/50 text-red-500 text-sm text-center">
+            {error}
+          </div>
+        )}
+
         {/* Register Form Content */}
-        <form className="mt-8 space-y-6" onSubmit={(e) => e.preventDefault()}>
+        <form className="mt-8 space-y-6" onSubmit={handleRegister}>
           <div className="space-y-4">
             {/* Input Name */}
             <div>
               <label className="block text-sm font-medium text-text-muted mb-1 ml-1">
                 Name
               </label>
-              <Input type="text" required placeholder="Your Name" />
+              <Input
+                type="text"
+                required
+                placeholder="Your Name"
+                value={formData.name}
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }
+              />
             </div>
             {/* Input Email */}
             <div>
               <label className="block text-sm font-medium text-text-muted mb-1 ml-1">
                 Email Address
               </label>
-              <Input type="email" required placeholder="name@example.com" />
+              <Input
+                type="email"
+                required
+                placeholder="name@example.com"
+                value={formData.email}
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                }
+              />
             </div>
 
             {/* Input Password */}
@@ -38,19 +94,37 @@ const RegisterPage = () => {
               <label className="block text-sm font-medium text-text-muted mb-1 ml-1">
                 Password
               </label>
-              <Input type="password" required placeholder="••••••••" />
+              <Input
+                type="password"
+                required
+                placeholder="••••••••"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData({ ...formData, password: e.target.value })
+                }
+              />
             </div>
             {/* Input Confirm Password */}
             <div>
               <label className="block text-sm font-medium text-text-muted mb-1 ml-1">
                 Confirm Password
               </label>
-              <Input type="password" required placeholder="••••••••" />
+              <Input
+                type="password"
+                required
+                placeholder="••••••••"
+                value={formData.confirmPassword}
+                onChange={(e) =>
+                  setFormData({ ...formData, confirmPassword: e.target.value })
+                }
+              />
             </div>
           </div>
 
           {/* Register Button */}
-          <Button type="submit">ĐĂNG KÝ</Button>
+          <Button type="submit" disabled={loading}>
+            {loading ? "ĐANG ĐĂNG KÝ..." : "ĐĂNG KÝ"}
+          </Button>
         </form>
 
         {/* Direct to Signup */}
